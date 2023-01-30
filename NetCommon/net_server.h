72102debff53e9b4ba13a1aff4a636f66a872b8d
Junt62 zi <Junt62@qq.com>
2023-01-30 22:29:06 +0800

    updated.

--- NetCommon/net_server.h
+++ NetCommon/net_server.h
@@ -15,7 +15,8 @@ namespace olc {
 
         public:
             server_interface(uint16_t port)
-                : m_asioAcceptor(m_asioContext, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), port)) {
+                : m_asioAcceptor(m_asioContext, boost::asio::ip::tcp::endpoint(
+                    boost::asio::ip::tcp::v4(), port)) {
 
             }
 
@@ -26,18 +27,19 @@ namespace olc {
             bool Start() {
 
                 try {
-
                     WaitForClientConnection();
-
+                    
                     m_threadContext = std::thread([this]() { m_asioContext.run(); });
                 }
                 catch (std::exception& e) {
                     // Something prohibited the server from listening
                     std::cerr << "[SERVER] Exception: " << e.what() << "\n";
+
                     return false;
                 }
 
                 std::cout << "[SERVER] Started!\n";
+
                 return true;
             }
 
@@ -54,6 +56,7 @@ namespace olc {
 
             // ASYNC - Instruct asio to wait for connection
             void WaitForClientConnection() {
+
                 m_asioAcceptor.async_accept(
                     [this](std::error_code ec, boost::asio::ip::tcp::socket socket) {
                         if (!ec) {
@@ -85,20 +88,73 @@ namespace olc {
                 // another conection...
                 WaitForClientConnection();
                     }
-                )
+                );
             }
 
             // Send a message to a specific client
             void MessageClient(std::shared_ptr<connection<T>> client, const message<T>& msg) {
-
+                
+                if (client && client->IsConnected()) {
+                    client->Send(msg);
+                }
+                else {
+                    OnClientDisconnect(client);
+                    client.reset();
+                    m_deqConnections.erase(
+                        std::remove(m_deqConnections.begin(), m_deqConnections.end(), client), m_deqConnections.end());
+                }
             }
 
             // Send message to all clients
             void MessageAllClients(const message<T>& msg, std::shared_ptr<connection<T>> pIgnoreClient = nullptr) {
+                
+                bool bInvalidClientExists = false;
 
+                for (auto& client : m_deqConnections) {
+
+                    // Check client is connected...
+                    if (client && client->IsConnected()) {
+
+                        // ...it is!
+                        if (client != pIgnoreClient)
+                            client->Send(msg);
+                    }
+                    else {
+
+                        // The Client couldn't be contacted, so assume it has disconnected.
+                        OnClientDisconnect(client);
+                        client.reset();
+
+                        // Set this flag to them remove dead clients from container
+                        bInvalidClientExists = true;
+                    }
+                }
+
+                // Remove dead clients, all in one go - this way, we dont invalidate the
+                // container as we iterated through it.
+                if (bInvalidClientExists)
+                    m_deqConnections.erase(
+                        std::remove(m_deqConnections.begin(), m_deqConnections.end(), nullptr), m_deqConnections.end());
+            }
+
+            void Update(sizt_t nMaxMessages = -1) {
+
+                size_t nMessageCount = 0;
+
+                while (nMessageCount < nMaxMessages && !m_qMessageIn.empty()) {
+
+                    // Grab the front message
+                    auto msg = m_qMessagesIn.pop_front();
+
+                    // Pass to message handler
+                    OnMessage(msg.remote, msg.msk);
+
+                    nMessageCount++;
+                }
             }
 
         protected:
+
             // Called when a client connects, you can veto the connection by returning false
             virtual bool OnClientConnect(std::shared_ptr<connection<T>> client) {
 
@@ -116,6 +172,7 @@ namespace olc {
             }
 
         protected:
+
             // Thread Safe Queue for incoming message packets
             tsqueue<owned_message<T>> m_qMessagesIn;
 
